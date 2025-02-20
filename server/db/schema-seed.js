7cb25e026c1032b7b061cd354434b9166a05f2f3
musserdn <musserdn@gmail.com>
2025-02-19 19:30:24 -0600

    fixed and validated the routes, removed some console logs no longer needed

--- server/db/schema-seed.js
+++ server/db/schema-seed.js
@@ -5,8 +5,7 @@ export const seedSchema = async () => {
   try {
     const schemaSQL = await readFile(new URL('./schema.sql', import.meta.url), 'utf8');
     await sequelize.query(schemaSQL);
-    console.log('\n----- SCHEMA CREATED -----\n');
-  } catch (error) {
+    } catch (error) {
     console.error('Error creating schema:', error);
   }
 };
