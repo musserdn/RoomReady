32c770a44b255ce6efa8bd2ed9086d5b60d2f645
musserdn <musserdn@gmail.com>
2025-02-19 19:11:48 -0600

    added name to the database as a column in users

--- server/db/seeds.sql
+++ server/db/seeds.sql
@@ -15,9 +15,10 @@ VALUES (1, 'Clean', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
        (12, 'Scheduled', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 
-INSERT INTO users (email, password, type, room_id, "createdAt", "updatedAt")
-VALUES ('nat@gmail.com', 'password', 'G', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-       ('dan@gmail.com', 'password', 'G', 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-       ('kevin@gmail.com', 'password', 'G', 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-       ('eric@gmail.com', 'password', 'H', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-       ('austin@gmail.com', 'password', 'H', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT INTO users (email, name, password, type, room_id, "createdAt", "updatedAt")
+VALUES ('nat@gmail.com', 'Nat', 'password', 'G', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('dan@gmail.com', 'Dan', 'password', 'G', 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('kevin@gmail.com', 'Kevin', 'password', 'G', 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('eric@gmail.com', 'Eric', 'password', 'H', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('austin@gmail.com', 'Austin', 'password', 'H', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+       ('kim@gmail.com', 'Kim', 'password', 'G', 4, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
