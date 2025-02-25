10fd18a360ae7da05180c17c986cb5094468199c
musserdn <musserdn@gmail.com>
2025-02-24 18:23:53 -0600

    render

--- server/src/server.js
+++ server/src/server.js
@@ -6,30 +6,31 @@ import routes from './routes/index.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Serve static files from the React app's dist directory
-app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// Serves static files in the entire client's dist folder
+app.use(express.static('../client/dist'));
 app.use(cors());
 app.use(express.json());
 app.use('/', routes);
 
-// Catch-all route to serve index.html for all other routes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
-});
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+  });
 
 // Global error handler
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: 'Internal Server Error' });
+    console.error(err.stack);
+    res.status(500).json({ message: 'Internal Server Error' });
 });
 
 sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-  });
+    app.listen(PORT, () => {
+        console.log(`Server is listening on port ${PORT}`);
+    });
 });
