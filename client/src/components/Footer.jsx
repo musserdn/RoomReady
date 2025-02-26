1f6aa3bd6307b1062404b25f4201ffc3b3d7a153
musserdn <musserdn@gmail.com>
2025-02-25 18:47:46 -0600

    added nav buttons to footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,19 @@
+import { Grid3x3, UserCog, Grid2x2Plus } from 'lucide-react';
+
 function Footer() {
     return (
         <footer className="container text-center py-5">
+            <div className="d-flex justify-content-center mb-3">
+                <a href="/HouseKeeping" className="mx-2">
+                    <Grid3x3 color="#D19C1D" size={30} strokeWidth={2.5} />
+                </a>
+                <a href="/user" className="mx-2">
+                    <UserCog color="#D19C1D" size={30} strokeWidth={2.5} />
+                </a>
+                <a href="/room" className="mx-2">
+                    <Grid2x2Plus color="#D19C1D" size={30} strokeWidth={2.5} />
+                </a>
+            </div>
             <div className="d-flex justify-content-center">
                 <p>&copy; Copyright 2025</p>
             </div>
